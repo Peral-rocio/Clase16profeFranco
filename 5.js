885be2026e3ad7bdf4543e2387f62bf13f5932e9
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-14 21:24:26 -0300

    ejercicio 5

---
+++ 5.js
@@ -0,0 +1,17 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 5: Recorre una lista e imprime cada elemento
+// Tienes una lista de frutas ['manzana', 'banana', 'cereza']. Tu tarea es recorrer la lista y mostrar el nombre de cada fruta en la consola, una por una.
+
+const frutas = [
+    "manzana",
+    "banana",
+    "pera",
+    "kiwi",
+    "sandia",
+    "durazno"
+]
+
+frutas.forEach(function (pais){
+    console.log(pais);
+} )
